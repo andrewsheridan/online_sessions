ad889be7f5333edc763b7e52133316cd1f54c69c
Andrew Sheridan <sheridan.andrew13@gmail.com>
2024-12-03 12:35:20 -0700

    Google sign in started

--- lib/bloc/firebase_auth_provider.dart
+++ lib/bloc/firebase_auth_provider.dart
@@ -2,16 +2,26 @@ import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:logging/logging.dart';
 
 class FirebaseAuthProvider extends ChangeNotifier {
   final Logger _logger = Logger("FirebaseAuthProvider");
   final FirebaseAuth _instance;
+  final String? _clientID;
+
+  late final GoogleSignIn _googleSignIn = GoogleSignIn(
+    scopes: ['email'],
+    clientId: _clientID,
+  );
 
   late final StreamSubscription _userChangedSubscription;
 
-  FirebaseAuthProvider({required FirebaseAuth instance})
-      : _instance = instance {
+  FirebaseAuthProvider({
+    required FirebaseAuth instance,
+    required String? clientID,
+  })  : _instance = instance,
+        _clientID = clientID {
     _userChangedSubscription = _instance.userChanges().listen(
           _handleUserChanged,
         );
@@ -38,62 +48,59 @@ class FirebaseAuthProvider extends ChangeNotifier {
     return cred.user!;
   }
 
-  Future<void> createUserWithEmail(String email, String password) async {
-    try {
-      await _instance.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-    } catch (ex) {
-      _logger.severe(
-        "Failed to create user at email address $email.",
-        ex,
-      );
-    }
-  }
-
-  Future<void> signInWithEmail(String email, String password) async {
-    try {
-      await _instance.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-    } catch (ex) {
-      _logger.severe("Failed to log in user $email.", ex);
-    }
-  }
-
-  // Future<void> googleSignIn() async {
-  //   final client = http.Client();
+  // Future<void> createUserWithEmail(String email, String password) async {
   //   try {
-  //     // Trigger the authentication flow
-  //     final clientID = ClientId(_clientID);
-
-  //     final credentials = await obtainAccessCredentialsViaUserConsent(
-  //       clientID,
-  //       ["email"],
-  //       client,
-  //       (prompt) {
-  //         launchUrlString(prompt);
-  //       },
+  //     await _instance.createUserWithEmailAndPassword(
+  //       email: email,
+  //       password: password,
   //     );
-  //     // Obtain the auth details from the request
-
-  //     // Create a new credential
-  //     final credential = GoogleAuthProvider.credential(
-  //       accessToken: credentials.accessToken.data,
-  //       idToken: credentials.idToken,
+  //   } catch (ex) {
+  //     _logger.severe(
+  //       "Failed to create user at email address $email.",
+  //       ex,
   //     );
-  //     // Once signed in, return the UserCredential
-  //     await _instance.signInWithCredential(credential);
-
-  //     _logger.info("Successfully signed in user.");
-  //   } on Exception catch (ex) {
-  //     _logger.severe("Failed to log in with google.", ex);
-  //   } finally {
-  //     client.close();
   //   }
   // }
+
+  // Future<void> signInWithEmail(String email, String password) async {
+  //   try {
+  //     await _instance.signInWithEmailAndPassword(
+  //       email: email,
+  //       password: password,
+  //     );
+  //   } catch (ex) {
+  //     _logger.severe("Failed to log in user $email.", ex);
+  //   }
+  // }
+
+  Future<UserCredential> googleSignIn() async {
+    try {
+      // Trigger the authentication flow
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // Once signed in, return the UserCredential
+      final output = await FirebaseAuth.instance.signInWithCredential(
+        credential,
+      );
+
+      _logger.info("Successfully signed in user.");
+
+      return output;
+    } on Exception catch (ex) {
+      _logger.severe("Failed to log in via Google.", ex);
+      rethrow;
+    }
+  }
 
   Future<UserCredential> signInAnonymously() async {
     try {
