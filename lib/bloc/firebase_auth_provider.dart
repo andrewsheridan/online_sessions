74d4f847c126aead0f653d424b78d6b2a3a065cd
Andrew Sheridan <sheridan.andrew13@gmail.com>
2025-01-22 14:48:33 -0700

    Apple sign in.

--- lib/bloc/firebase_auth_provider.dart
+++ lib/bloc/firebase_auth_provider.dart
@@ -1,9 +1,14 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
 
+import 'package:crypto/crypto.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:logging/logging.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 class FirebaseAuthProvider extends ChangeNotifier {
   final Logger _logger = Logger("FirebaseAuthProvider");
@@ -124,6 +129,64 @@ class FirebaseAuthProvider extends ChangeNotifier {
       return output;
     } catch (ex) {
       _logger.severe("Anonymous sign in failed.", ex);
+      rethrow;
+    }
+  }
+
+  /// Generates a cryptographically secure random nonce, to be included in a
+  /// credential request.
+  String generateNonce([int length = 32]) {
+    const charset =
+        '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
+    final random = Random.secure();
+    return List.generate(length, (_) => charset[random.nextInt(charset.length)])
+        .join();
+  }
+
+  /// Returns the sha256 hash of [input] in hex notation.
+  String sha256ofString(String input) {
+    final bytes = utf8.encode(input);
+    final digest = sha256.convert(bytes);
+    return digest.toString();
+  }
+
+  Future<void> signInWithApple() async {
+    try {
+      final appleProvider = AppleAuthProvider();
+      if (kIsWeb) {
+        await FirebaseAuth.instance.signInWithPopup(appleProvider);
+      } else if (Platform.isIOS || Platform.isMacOS || Platform.isAndroid) {
+        await FirebaseAuth.instance.signInWithProvider(appleProvider);
+      } else {
+        // Pulled from https://dev.to/offlineprogrammer/flutter-firebase-authentication-apple-sign-in-1m64
+
+        // To prevent replay attacks with the credential returned from Apple, we
+        // include a nonce in the credential request. When signing in in with
+        // Firebase, the nonce in the id token returned by Apple, is expected to
+        // match the sha256 hash of `rawNonce`.
+        final rawNonce = generateNonce();
+        final nonce = sha256ofString(rawNonce);
+        // Request credential for the currently signed in Apple account.
+        final appleCredential = await SignInWithApple.getAppleIDCredential(
+          scopes: [
+            AppleIDAuthorizationScopes.email,
+            AppleIDAuthorizationScopes.fullName,
+          ],
+          nonce: nonce,
+        );
+
+        // Create an `OAuthCredential` from the credential returned by Apple.
+        final oauthCredential = OAuthProvider("apple.com").credential(
+          idToken: appleCredential.identityToken,
+          rawNonce: rawNonce,
+        );
+
+        // Sign in the user with Firebase. If the nonce we generated earlier does
+        // not match the nonce in `appleCredential.identityToken`, sign in will fail.
+        await _instance.signInWithCredential(oauthCredential);
+      }
+    } catch (exception) {
+      _logger.severe("Failed to login with Apple.", exception);
       rethrow;
     }
   }
